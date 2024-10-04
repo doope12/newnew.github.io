255026c6e74ad70e0d0991ca812e5ba2d0ee7c14
Rafał Klocek <rafal.klocek.praca@gmail.com>
2024-10-04 16:35:50 +0200

    added upgrader it might have some bugs

--- src/js/modes/upgrader.js
+++ src/js/modes/upgrader.js
@@ -86,7 +86,7 @@ const items1 = {
 		skin: "Mudder",
 		color: "gold",
 		imgDist: "../dist/img/weapons/knives/classic/safari-mesh.jpg",
-		price: 175.0,
+		price: 175.42,
 		id: 9,
 	},
 	id10: {
@@ -98,11 +98,309 @@ const items1 = {
 		price: 611.78,
 		id: 10,
 	},
+	id11: {
+		weapon: "Desert Eagle",
+		name: "Desert Eagle Printstream",
+		skin: "Tiger Tooth",
+		color: "red",
+		imgDist: "../dist/img/weapons/pistols/desert-eagle/printstream.jpg",
+		price: 85.72,
+		id: 11,
+	},
+	id12: {
+		weapon: "USP-S",
+		name: "USP-S Printstream",
+		skin: "Printstream",
+		color: "red",
+		imgDist: "../dist/img/weapons/pistols/usp-s/printstream.jpg",
+		price: 109.15,
+		id: 12,
+	},
+	id13: {
+		weapon: "AK-47",
+		name: "AK-47 Asiimov",
+		skin: "Asiimov",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/ak-47/asiimov.jpg",
+		price: 171.27,
+		id: 13,
+	},
+	id14: {
+		weapon: "AWP",
+		name: "AWP Neo-Noir",
+		skin: "Neo-Noir",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/awp/neo-noir.jpg",
+		price: 28.92,
+		id: 14,
+	},
+	id15: {
+		weapon: "AK-47",
+		name: "AK-47 Inheritance",
+		skin: "Inheritance",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/ak-47/inheritance.jpg",
+		price: 154.53,
+		id: 15,
+	},
+	id16: {
+		weapon: "AK-47",
+		name: "AK-47 Nightwish",
+		skin: "Nightwish",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/ak-47/nightwish.jpg",
+		price: 61.27,
+		id: 16,
+	},
+	id17: {
+		weapon: "AK-47",
+		name: "AK-47 Ice Coaled",
+		skin: "Ice Coaled",
+		color: "pink",
+		imgDist: "../dist/img/weapons/rifles/ak-47/ice-coaled.jpg",
+		price: 14.52,
+		id: 17,
+	},
+	id18: {
+		weapon: "M4A4",
+		name: "M4A4 Temukau",
+		skin: "Temukau",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/m4a4/temukau.jpg",
+		price: 99.24,
+		id: 18,
+	},
+	id19: {
+		weapon: "M4A1-S",
+		name: "M4A1-S Decimator",
+		skin: "Decimator",
+		color: "pink",
+		imgDist: "../dist/img/weapons/rifles/m4a1-s/decimator.jpg",
+		price: 39.84,
+		id: 19,
+	},
+	id20: {
+		weapon: "M4A1-S",
+		name: "M4A1-S Nightmare",
+		skin: "Nightmare",
+		color: "pink",
+		imgDist: "../dist/img/weapons/rifles/m4a1-s/nightmare.jpg",
+		price: 44.09,
+		id: 20,
+	},
+	id21: {
+		weapon: "AWP",
+		name: "AWP Duality",
+		skin: "Duality",
+		color: "pink",
+		imgDist: "../dist/img/weapons/rifles/awp/duality.jpg",
+		price: 9.22,
+		id: 21,
+	},
+	id22: {
+		weapon: "Butterfly",
+		name: "Butterfly Freehand",
+		skin: "Freehand",
+		color: "gold",
+		imgDist: "../dist/img/weapons/knives/butterfly/freehand.jpg",
+		price: 999.15,
+		id: 22,
+	},
+	id23: {
+		weapon: "Shadow Daggers",
+		name: "Shadow Daggers Bright Water",
+		skin: "Bright Water",
+		color: "gold",
+		imgDist: "../dist/img/weapons/knives/shadow-daggers/bright-water.jpg",
+		price: 92.1,
+		id: 23,
+	},
+	id24: {
+		weapon: "Huntsman",
+		name: "Huntsman Black Laminate",
+		skin: "Black Laminate",
+		color: "gold",
+		imgDist: "../dist/img/weapons/knives/huntsman/black-laminate.jpg",
+		price: 181.67,
+		id: 24,
+	},
+	id25: {
+		weapon: "Bayonet",
+		name: "Bayonet Doppler",
+		skin: "Doppler",
+		color: "gold",
+		imgDist: "../dist/img/weapons/knives/bayonet/doppler.jpg",
+		price: 572.92,
+		id: 25,
+	},
+	id26: {
+		weapon: "AWP",
+		name: "AWP Desert Hydra",
+		skin: "Desert Hydra",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/awp/desert-hydra.jpg",
+		price: 2327.58,
+		id: 26,
+	},
+	id27: {
+		weapon: "AWP",
+		name: "AWP Gungnir",
+		skin: "Gungnir",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/awp/gungnir.jpg",
+		price: 11709.26,
+		id: 27,
+	},
+	id28: {
+		weapon: "AWP",
+		name: "AWP Dragon Lore",
+		skin: "Dragon Lore",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/awp/dragon-lore.jpg",
+		price: 11025.82,
+		id: 28,
+	},
+	id29: {
+		weapon: "AWP",
+		name: "AWP Medusa",
+		skin: "Medusa",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/awp/medusa.jpg",
+		price: 4328.88,
+		id: 29,
+	},
+	id30: {
+		weapon: "AWP",
+		name: "AWP The Prince",
+		skin: "The Prince",
+		color: "red",
+		imgDist: "../dist/img/weapons/rifles/awp/the-prince.jpg",
+		price: 3207.63,
+		id: 30,
+	},
+	id31: {
+		weapon: "Driver Gloves",
+		name: "Driver Gloves Impreial Plaid",
+		skin: "Impreial Plaid",
+		color: "gold",
+		imgDist: "../dist/img/weapons/gloves/imperial-plaid.jpg",
+		price: 1213.36,
+		id: 31,
+	},
+	id32: {
+		weapon: "Sport Gloves",
+		name: "Sport Gloves Nocts",
+		skin: "Nocts",
+		color: "gold",
+		imgDist: "../dist/img/weapons/gloves/nocts.jpg",
+		price: 2254.99,
+		id: 32,
+	},
+	id33: {
+		weapon: "Specialist Gloves",
+		name: "Specialist Gloves Marble Fade",
+		skin: "Marble Fade",
+		color: "gold",
+		imgDist: "../dist/img/weapons/gloves/marble-fade.jpg",
+		price: 2639.76,
+		id: 33,
+	},
+	id34: {
+		weapon: "Hydra Gloves",
+		name: "Hydra Gloves Emerald",
+		skin: "Emerald",
+		color: "gold",
+		imgDist: "../dist/img/weapons/gloves/emerald.jpg",
+		price: 255.4,
+		id: 34,
+	},
+	id35: {
+		weapon: "P2000",
+		name: "P2000 Amber Fade",
+		skin: "Amber Fade",
+		color: "purple",
+		imgDist: "../dist/img/weapons/pistols/p2000/amber-fade.jpg",
+		price: 8.31,
+		id: 35,
+	},
+	id36: {
+		weapon: "Glock-18",
+		name: "Glock-18 Gamma Doppler",
+		skin: "Gamma Doppler",
+		color: "red",
+		imgDist: "../dist/img/weapons/pistols/glock-18/gamma-doppler.jpg",
+		price: 55.18,
+		id: 36,
+	},
+	id37: {
+		weapon: "Glock-18",
+		name: "Glock-18 Vogue",
+		skin: "Vogue",
+		color: "pink",
+		imgDist: "../dist/img/weapons/pistols/glock-18/vogue.jpg",
+		price: 7.12,
+		id: 37,
+	},
+	id38: {
+		weapon: "CZ75-Auto",
+		name: "CZ75-Auto Tuxedo",
+		skin: "Tuxedo",
+		color: "blue",
+		imgDist: "../dist/img/weapons/pistols/cz75-auto/tuxedo.jpg",
+		price: 0.95,
+		id: 38,
+	},
+	id39: {
+		weapon: "CZ75-Auto",
+		name: "CZ75-Auto Jungle Dashed",
+		skin: "Jungle Dashed",
+		color: "white",
+		imgDist: "../dist/img/weapons/pistols/cz75-auto/jungle-dashed.jpg",
+		price: 1.24,
+		id: 39,
+	},
+	id40: {
+		weapon: "R8 Revolver",
+		name: "R8 Revolver Amber Fade",
+		skin: "Amber Fade",
+		color: "pink",
+		imgDist: "../dist/img/weapons/pistols/r8-revolver/amber-fade.jpg",
+		price: 0.62,
+		id: 40,
+	},
+	id41: {
+		weapon: "R8 Revolver",
+		name: "R8 Revolver Phoenix Marker",
+		skin: "Phoenix Marker",
+		color: "light-blue",
+		imgDist: "../dist/img/weapons/pistols/r8-revolver/phoenix-marker.jpg",
+		price: 3.66,
+		id: 41,
+	},
+	id42: {
+		weapon: "R8 Revolver",
+		name: "R8 Revolver Crimson Web",
+		skin: "Crimson Web",
+		color: "blue",
+		imgDist: "../dist/img/weapons/pistols/r8-revolver/crimson-web.jpg",
+		price: 3.77,
+		id: 42,
+	},
+	id43: {
+		weapon: "R8 Revolver",
+		name: "R8 Revolver Memento",
+		skin: "Memento",
+		color: "purple",
+		imgDist: "../dist/img/weapons/pistols/r8-revolver/memento.jpg",
+		price: 1.55,
+		id: 43,
+	},
 };
 const itemUserOwnList = document.querySelector(".upgrader__list-playeritems");
 const itemAllList = document.querySelector(".upgrader__list-allitems");
 const selectUserItems = document.querySelector("#price-user");
 const selectAllItems = document.querySelector("#price-all");
+const innerCircle = document.querySelector(".upgrader__base-inner");
 const countItemsAmount = Object.keys(items).length;
 
 const addPlayerItems = () => {
@@ -135,6 +433,8 @@ const addPlayerItems = () => {
 			itemUserOwnList.append(itemBox);
 		}
 	}
+
+	setUserItemsOrder();
 };
 
 const addAllItems = () => {
@@ -164,6 +464,8 @@ const addAllItems = () => {
 		itemBox.append(itemImg, itemName, itemSkin, itemPrice);
 		itemAllList.append(itemBox);
 	}
+
+	setAllItemsOrder();
 };
 
 const setUserItemsOrder = () => {
@@ -218,6 +520,7 @@ function addToLeftSide() {
 	topLeftBox.id = this.id;
 
 	countPercent();
+	hideCheaperItems();
 }
 
 function addToRightSide() {
@@ -234,16 +537,52 @@ function addToRightSide() {
 	countPercent();
 }
 
+const hideCheaperItems = () => {
+	const allItems = document.querySelectorAll(".upgrader__item-allitems");
+	const topLeftBox = document.querySelector(".upgrader__top-left");
+
+	const userItemPrice = parseFloat(
+		topLeftBox.firstElementChild.textContent
+	).toFixed(2);
+
+	allItems.forEach((item) => {
+		const itemPrice = parseFloat(item.lastElementChild.textContent).toFixed(2);
+
+		if (parseFloat(itemPrice) <= parseFloat(userItemPrice)) {
+			item.style.display = "none";
+		} else {
+			item.style.display = "flex";
+		}
+	});
+};
+
+const clearLeftSide = () => {
+	const infoBox = document.querySelector(".upgrader__infobox-left");
+	const topLeftBox = document.querySelector(".upgrader__top-left");
+
+	infoBox.firstElementChild.textContent = "0.00$";
+	topLeftBox.lastElementChild.setAttribute("src", "../dist/img/blank.png");
+	topLeftBox.id = "";
+
+	countPercent();
+};
+
 const countPercent = () => {
 	const infoBoxLeft = document.querySelector(".upgrader__infobox-left");
 	const infoBoxRight = document.querySelector(".upgrader__infobox-right");
-	const priceLeft = infoBoxLeft.firstElementChild.textContent;
-	const priceRight = infoBoxRight.firstElementChild.textContent;
+	const priceLeft = infoBoxLeft.firstElementChild.textContent.replace("$", "");
+	const priceRight = infoBoxRight.firstElementChild.textContent.replace(
+		"$",
+		""
+	);
 
 	const circle = document.querySelector(".upgrader__base");
 	const percentageText = document.querySelector(".upgrader__percent");
 
-	if (priceLeft !== "0.00$" && priceRight !== "0.00$") {
+	circle.style.transform = "rotate(0deg)";
+	circle.style.transition = "0.01s";
+
+	if (priceLeft !== "0.00" && priceRight !== "0.00") {
 		const percentForWin =
 			(parseFloat(priceLeft) / parseFloat(priceRight)) * 100;
 
@@ -254,17 +593,110 @@ const countPercent = () => {
 			percentageText.textContent = percentForWin.toFixed(2) + "%";
 			circle.style.background = `conic-gradient(#cfa0ff 0 ${percentForWin}%, #292a2d ${percentForWin}% 100%)`;
 		}
+	} else {
+		percentageText.textContent = "0.00%";
+		circle.style.background = `conic-gradient(#cfa0ff 0 0%, #292a2d 0 100%)`;
 	}
 };
+
+// Funkcja losująca liczbę i określająca wynik (wygrana/przegrana)
+const determineWin = (percentForWin) => {
+	// Losujemy liczbę z zakresu 0 - 9999
+	const randomNumber = Math.floor(Math.random() * 10000);
+
+	// Jeśli wylosowana liczba jest mniejsza niż szansa na wygraną * 100 (bo procenty są w zakresie 0 - 100)
+	return randomNumber < percentForWin * 100; // True = wygrana, False = przegrana
+};
+
+const spinCircle = () => {
+	const circle = document.querySelector(".upgrader__base");
+	circle.style.transition = "5s ease-out";
+
+	// Pobierz procent szansy na wygraną
+	const percentForWin = parseFloat(
+		document.querySelector(".upgrader__percent").textContent
+	);
+
+	// Losowanie wygranej
+	const isWin = determineWin(percentForWin);
+
+	// Losowanie liczby pełnych obrotów (np. 3-6 obrotów)
+	const randomSpins = Math.floor(Math.random() * 3) + 2;
+
+	// Kąt docelowy zależny od wygranej lub przegranej
+	let targetAngle;
+
+	if (isWin) {
+		// Wygrana - koło ma zatrzymać się na kolorze (obszar procentowy wygranej)
+		const winningAngleThreshold = (percentForWin / 100) * 360;
+		const fromWhatAngle = 360 - winningAngleThreshold;
+		targetAngle = Math.random() * winningAngleThreshold + fromWhatAngle;
+	} else {
+		// Przegrana - koło ma zatrzymać się na czarnym obszarze
+		const losingAngleThreshold = (percentForWin / 100) * 360;
+		targetAngle = Math.random() * (360 - losingAngleThreshold);
+	}
+
+	// Całkowity obrót: pełne obroty + losowy kąt docelowy (na odpowiedni obszar)
+	const totalRotation = randomSpins * 360 + targetAngle;
+
+	// Obracamy koło
+	circle.style.transform = `rotate(${totalRotation}deg)`;
+
+	// Wyświetl w konsoli wynik
+	setTimeout(() => {
+		addWinningItems(isWin);
+	}, 5000); // Czas trwania animacji (5s)
+};
+
+const addWinningItems = (didWin) => {
+	const topLeftBox = document.querySelector(".upgrader__top-left");
+	const topRightBox = document.querySelector(".upgrader__top-right");
+
+	const removeUpgradedItem =
+		parseInt(localStorage.getItem(`id${topLeftBox.id}`)) - 1;
+	localStorage.setItem(`id${topLeftBox.id}`, removeUpgradedItem);
+
+	if (didWin) {
+		if (
+			localStorage.getItem(`id${topRightBox.id}`) === null ||
+			localStorage.getItem(`id${topRightBox.id}`) === NaN
+		) {
+			localStorage.setItem(`id${topRightBox.id}`, 1);
+		} else {
+			const itemToAdd =
+				parseInt(localStorage.getItem(`id${topRightBox.id}`)) + 1;
+			localStorage.setItem(`id${topRightBox.id}`, itemToAdd);
+		}
+	}
+
+	clearLeftSide();
+	itemUserOwnList.innerHTML = "";
+	itemAllList.innerHTML = "";
+
+	addPlayerItems();
+	addAllItems();
+
+	const userItems = document.querySelectorAll(".upgrader__item-useritems");
+	const allItems = document.querySelectorAll(".upgrader__item-allitems");
+
+	userItems.forEach((item) => {
+		item.addEventListener("click", addToLeftSide);
+	});
+
+	allItems.forEach((item) => {
+		item.addEventListener("click", addToRightSide);
+	});
+};
+
+countPercent();
 
 addPlayerItems();
 addAllItems();
 
-setUserItemsOrder();
-setAllItemsOrder();
-
 selectAllItems.addEventListener("change", setAllItemsOrder);
 selectUserItems.addEventListener("change", setUserItemsOrder);
+innerCircle.addEventListener("click", spinCircle);
 
 const addListenersToItems = () => {
 	const userItems = document.querySelectorAll(".upgrader__item-useritems");
